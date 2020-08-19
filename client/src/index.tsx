438adfc89b91e59944fb2fd4525fafedcbc64f0d
vinaya22 <49248059+vinaya22@users.noreply.github.com>
2020-08-19 16:06:24 +0530

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,1 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
+const Component = '<div>My-App</div>';
+
+ReactDOM.render(   
+    <Component/>,
+    document.getElementById('root')
+);
