8a4e84bd9047122fc515b92e8e28520874619db1
vinaya22 <49248059+vinaya22@users.noreply.github.com>
2020-08-19 16:01:39 +0530

    Create webpack.config.js

---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      template: 'src/index.html'
+    }),
+  ]
+};
