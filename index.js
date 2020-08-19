e8e7ae95c4e84611fab84c0f069692d70dd50c13
vinaya22 <49248059+vinaya22@users.noreply.github.com>
2020-08-19 15:24:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'index.html')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
